0dff600a6485f412d2869b3b5a8d00a5666bca1f
alanchiem <chiemalan@gmail.com>
2023-01-26 10:51:34 -0800

    assignment complete

--- quad_test.cpp
+++ quad_test.cpp
@@ -19,7 +19,8 @@ unsigned int texture;
  */
 Vector3D mult(Matrix3x3 mat, Vector3D input) {
     /* TODO */
-    return input;
+
+    return mat * input;
 }
 
 class QuadDrawer : public Renderer {
@@ -53,8 +54,8 @@ class QuadDrawer : public Renderer {
       GL_NEAREST_MIPMAP_LINEAR
       GL_LINEAR_MIPMAP_LINEAR
      */
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     // load and generate the texture
     int width, height, nrChannels;
     //TODO: (optional) Change the picture here!
@@ -90,13 +91,13 @@ class QuadDrawer : public Renderer {
     glTexCoord2f(0,0);
     glVertex3f(a_trans[0], a_trans[1], a_trans[2]);
     /* TODO: change the (0,1) below to (0,.1) to zoom into the texture to see changes. */
-    glTexCoord2f(0,1);
+    glTexCoord2f(0,.1);
     glVertex3f(b_trans[0], b_trans[1], b_trans[2]);
     /* TODO: change the (1,1) below to (.1,.1) to zoom into the texture to see changes. */
-    glTexCoord2f(1,1);
+    glTexCoord2f(.1,.1);
     glVertex3f(c_trans[0], c_trans[1], c_trans[2]);
     /* TODO: change the (1,0) to (.1,0) to zoom into the texture to see changes. */
-    glTexCoord2f(1,0);
+    glTexCoord2f(.1,0);
     glVertex3f(d_trans[0], d_trans[1], d_trans[2]);
     glDeleteTextures(1, &texture);
     glDisable(GL_TEXTURE_2D);
